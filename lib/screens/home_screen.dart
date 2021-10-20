204fa7f3f674f4e856f4db361c6b351190f73343
Arthur Cezar <arthurcvm@hotmail.com>
2021-10-20 17:45:43 -0300

    feat: create home background & appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/tabs/home_tab.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -7,14 +8,12 @@ final _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    
+
     return PageView(
       controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-        Container(color: Colors.yellow),
-        Container(color: Colors.red,),
-        Container(color: Colors.green,)
+        HomeTab()
       ]
     );
   }
